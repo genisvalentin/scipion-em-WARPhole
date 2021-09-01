24681ca586c01993f87e2232fb55ac47d0e0290b
genval <genis.valentin.gese@ki.se>
2021-09-01 13:17:31 +0200

    Switched to class inheritance

--- WARPhole/protocols/protocol_move_to_stcratch.py
+++ WARPhole/protocols/protocol_move_to_stcratch.py
@@ -50,8 +50,9 @@ from pyworkflow import VERSION_2_0
 from pwem.protocols import EMProtocol
 from pyworkflow.object import Set
 from pyworkflow.protocol.params import BooleanParam, IntParam, PointerParam, GT, FolderParam
+from xmipp3.protocols.protocol_trigger_data import XmippProtTriggerData
 
-class MoveToScratch(EMProtocol):
+class MoveToScratch(XmippProtTriggerData):
     """
     Waits until certain number of images is prepared and then
     send them to output.
@@ -108,32 +109,6 @@ class MoveToScratch(EMProtocol):
                       help="Delay in seconds before checking new output")
 
     # --------------------------- INSERT steps functions ----------------------
-    def _insertAllSteps(self):
-
-        if self.revert:
-            self.splitImages = False
-            self.allImages = True
-
-        # initializing variables
-        self.finished = False
-        self.images = []
-        self.splitedImages = []
-        self.outputCount = 0
-        self.setImagesClass()
-        self.setImagesType()
-
-        # steps
-        imsSteps = self._insertFunctionStep('delayStep')
-        self._insertFunctionStep('createOutputStep',
-                                 prerequisites=[imsSteps], wait=True)
-
-    def _stepsCheck(self):
-        self._checkNewInput()
-        self._checkNewOutput()
-
-    def createOutputStep(self):
-        pass
-
 
     def _checkNewInput(self):
         imsFile = self.inputImages.get().getFileName()
@@ -176,141 +151,6 @@ class MoveToScratch(EMProtocol):
         # filling the output if needed
         self._fillingOutput()
 
-    def _checkNewOutput(self):
-        if getattr(self, 'finished', False):
-            return
-
-        if self.streamClosed:
-            self.finished = True
-        elif not self.allImages.get():
-            self.finished = len(self.images) >= self.outputSize
-        else:
-            self.finished = False
-
-        outputStep = self._getFirstJoinStep()
-        deps = []
-        if self.finished:  # Unlock createOutputStep if finished all jobs
-            self._fillingOutput()  # To do the last filling
-            if outputStep and outputStep.isWaiting():
-                outputStep.setStatus(cons.STATUS_NEW)
-        else:
-            delayId = self._insertFunctionStep('delayStep', prerequisites=[])
-            deps.append(delayId)
-
-        if outputStep is not None:
-            outputStep.addPrerequisites(*deps)
-        self.updateSteps()
-
-    def _fillingOutput(self):
-        imsSqliteFn = '%s.sqlite' % self.getImagesType('lower')
-        outputName = self.getOututName()
-        if len(self.images) >= self.outputSize or self.finished:
-            if self.allImages:  # Streaming and semi-streaming
-                if self.splitImages:  # Semi-streaming: Splitting the input
-                    if len(self.splitedImages) >= self.outputSize or \
-                            (self.finished and len(self.splitedImages) > 0):
-                        self.outputCount += 1
-                        imageSet = self._loadOutputSet(self.getImagesClass(),
-                                                       '%s%d.sqlite'
-                                                       % (self.getImagesType('lower'),
-                                                          self.outputCount),
-                                                       self.splitedImages)
-                        # The splitted outputSets are always closed
-                        self._updateOutputSet("%s%d" % (outputName, self.outputCount),
-                                              imageSet, Set.STREAM_CLOSED)
-                        self.splitedImages = []
-                else:  # Full streaming case
-                    if not os.path.exists(self._getPath(imsSqliteFn)):
-                        imageSet = self._loadOutputSet(self.getImagesClass(),
-                                                       imsSqliteFn,
-                                                       self.images)
-                    else:
-                        # if finished no images to add, but we need to close the set
-                        imagesToAdd = self.newImages if not self.finished else []
-                        imageSet = self._loadOutputSet(self.getImagesClass(),
-                                                       imsSqliteFn,
-                                                       imagesToAdd)
-                    streamMode = Set.STREAM_CLOSED if self.finished else \
-                        Set.STREAM_OPEN
-                    self._updateOutputSet(outputName, imageSet, streamMode)
-
-            elif not os.path.exists(self._getPath(imsSqliteFn)):
-                imageSet = self._loadOutputSet(self.getImagesClass(), imsSqliteFn,
-                                               self.images)
-                # The outputSet is always closed here
-                self._updateOutputSet(outputName, imageSet, Set.STREAM_CLOSED)
-
-    def _loadOutputSet(self, SetClass, baseName, newImages):
-        setFile = self._getPath(baseName)
-        if os.path.exists(setFile):
-            outputSet = SetClass(filename=setFile)
-            outputSet.loadAllProperties()
-            outputSet.enableAppend()
-        else:
-            outputSet = SetClass(filename=setFile)
-            outputSet.setStreamState(outputSet.STREAM_OPEN)
-
-        inputs = self.inputImages.get()
-        outputSet.copyInfo(inputs)
-        outputSet.copyItems(newImages)
-        return outputSet
-
-    def _updateOutputSet(self, outputName, outputSet, state=Set.STREAM_OPEN):
-        outputSet.setStreamState(state)
-        if self.hasAttribute(outputName):
-            outputSet.write()  # Write to commit changes
-            outputAttr = getattr(self, outputName)
-            # Copy the properties to the object contained in the protocol
-            outputAttr.copy(outputSet, copyId=False)
-            # Persist changes
-            self._store(outputAttr)
-        else:
-            self._defineOutputs(**{outputName: outputSet})
-            self._defineTransformRelation(self.inputImages, outputSet)
-            self._store(outputSet)
-
-        # Close set database to avoid locking it
-        outputSet.close()
-
-    # --------------------------- INFO functions ------------------------------
-    def _summary(self):
-        summary = []
-
-        outputStr = self.getOututName()
-
-        if self.allImages.get() and not self.splitImages.get():
-            summary.append("MODE: *full streaming*.")
-            triggeredMsg = ("'%s' released, it will be growing up "
-                            "as soon as the input does." % outputStr)
-        elif self.splitImages.get():
-            summary.append("MODE: *semi streaming (batches)*.")
-            triggeredMsg = ("%d '%s' are being released with %d items, "
-                            "each. A new batch will be created when ready."
-                            % (self.getOutputsSize(), outputStr, self.outputSize))
-        else:
-            summary.append("MODE: *static output*.")
-            triggeredMsg = ("'%s' released and closed. Nothing else to do."
-                            % outputStr)
-
-        if self.getOutputsSize():
-            summary.append(triggeredMsg)
-        else:
-            inputStr = self.getImagesType() if self.inputImages.get() else '(or not ready)'
-            summary.append("Not enough input %s to release an output, yet."
-                           % inputStr)
-            summary.append("At least, %d items are needed to trigger an output."
-                           % self.outputSize.get())
-
-        if (self.isFinished() and
-                self.outputSize.get() > [o for o in
-                                         self.iterOutputAttributes()][0][1].getSize()):
-            summary.append("Output released because streaming finished.")
-
-        return summary
-
-    def _validate(self):
-        pass
-
     def _moveImages(self,imgSet):
         scratchPath = str(self.scratchPath)
         imgSetSize = self._getImgSetSize(imgSet)
@@ -349,46 +189,3 @@ class MoveToScratch(EMProtocol):
 
     def _getFreeScratchSpace(self,path):
         return(shutil.disk_usage(path).free)
-
-    # --------------------------- UTILS functions -----------------------------
-    def _getFirstJoinStepName(self):
-        # This function will be used for streaming, to check which is
-        # the first function that need to wait for all micrographs
-        # to have completed, this can be overriden in subclasses
-        # (e.g., in Xmipp 'sortPSDStep')
-        return 'createOutputStep'
-
-    def _getFirstJoinStep(self):
-        for s in self._steps:
-            if s.funcName == self._getFirstJoinStepName():
-                return s
-        return None
-
-    def delayStep(self):
-        time.sleep(self.delay)
-
-    def setImagesClass(self):
-        self._inputClass = self.inputImages.get().getClass()
-
-    def getImagesClass(self):
-        return self._inputClass
-
-    def setImagesType(self):
-        inputSet = self.inputImages.get()
-        if inputSet:
-            inputClassName = inputSet.getClassName()
-            self._inputType = inputClassName.split('SetOf')[1]
-        else:
-            self._inputType = None
-
-    def getImagesType(self, letters='default'):
-        if not self.hasAttribute('_inputType'):
-            self.setImagesType()
-        typeStr = str(self._inputType)
-        if letters == 'lower':
-            return typeStr.lower()
-        else:
-            return typeStr
-
-    def getOututName(self):
-        return 'output%s' % self.getImagesType()
