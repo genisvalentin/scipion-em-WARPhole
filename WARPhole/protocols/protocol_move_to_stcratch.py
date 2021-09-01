262df24c181197af6365614f072e20bb46b17d3b
genval <genis.valentin.gese@ki.se>
2021-09-01 13:35:06 +0200

    More cleaning up

--- WARPhole/protocols/protocol_move_to_stcratch.py
+++ WARPhole/protocols/protocol_move_to_stcratch.py
@@ -1,23 +1,9 @@
-'''
-create an empty particle stack
-read open particle set and get at list of new stacks
-if new stacks, iterate through the list of new stacks and insertfunctionstep to move each stack
-	get the following particle stack, if there is none, go back to sleep
-	check if there is enough disk space for the destination stacks
-		if not enough space, stop here
-	put the particles in the stack into a loading port
-	spawn a new copy operation for the next stack
-	when copy finished, make the new particle stack available
-else
-	sleep
-go back to 2
-'''
+# -*- coding: utf-8 -*-
 # **************************************************************************
 # *
-# * Authors:     Tomas Majtner (tmajtner@cnb.csic.es)
-# *              David Maluenda (dmaluenda@cnb.csic.es)
+# * Authors:     Genis Valentin Gese (genis.valentin.gese@ki.se)
 # *
-# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# * Karolinska Institutet
 # *
 # * This program is free software; you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License as published by
@@ -35,10 +21,9 @@ go back to 2
 # * 02111-1307  USA
 # *
 # *  All comments concerning this program package may be sent to the
-# *  e-mail address 'scipion@cnb.csic.es'
+# *  e-mail address 'genis.valentin.gese@ki.se'
 # *
 # **************************************************************************
-
 import os
 import time
 from datetime import datetime
@@ -167,7 +152,6 @@ class MoveToScratch(XmippProtTriggerData):
             print("Not enough scratch space available. Sleeping for 60 seconds")
         for img in imgSet:
             filename = img.getFileName()
-            path,name = os.path.dirname(filename),os.path.basename(filename)
             newFilename = os.path.join(scratchPath,filename)
             pwutils.path.makeFilePath(newFilename)
             symlink = self._getExtraPath(filename)
