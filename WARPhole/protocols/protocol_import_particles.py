d7a28bb7caa8275b07234f158957ed9517f332b5
genval <genis.valentin.gese@ki.se>
2021-11-30 15:24:30 +0100

    Cleanup code

--- WARPhole/protocols/protocol_import_particles.py
+++ WARPhole/protocols/protocol_import_particles.py
@@ -192,7 +192,6 @@ class WARPholeImportParticles(EMProtocol):
                 #pass
             #if self.streamingHasFinished():
             finish = self.streamingHasFinished()
-            self.info("Checking if streaming has finished {}".format(str(finish)))
             if not finish:
                 time.sleep(self.fileTimeout.get())
             #First we update and close the streaming of the datasets
